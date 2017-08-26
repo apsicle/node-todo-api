f78abdc13e325e073d94ba420aa7d918ecd872b7
apsicle <rcyan@email.wm.edu>
2017-08-26 19:29:42 -0400

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {User} = require('./models/user.js');
 var {Todo} = require('./models/todo.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 // bodyParser... passes key-value pairs from the post request into request.body. Q: why
 // isn't this done automatically by express?
@@ -57,8 +58,8 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log(`Started on port 3000`);
+app.listen(port, () => {
+	console.log(`Started on port ${port}`);
 });
 
 
